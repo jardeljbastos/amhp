423776b759eec79e7c0dbbcb573d6f94456c3126
realidademax <84300151+realidademax@users.noreply.github.com>
2021-06-03 22:19:54 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salaexamei-ge-nm830",
+      "name": "SalaExameI GE NM830",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.30804263583837965,
+        "pitch": 0.7023771885596979,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.756370852141953,
+          "pitch": 0.4847049160265158,
+          "rotation": 0,
+          "target": "1-salaexameii-ge-nm830"
+        },
+        {
+          "yaw": 2.91298800149994,
+          "pitch": 0.2817487063106281,
+          "rotation": 0,
+          "target": "2-salacomando-ge-nm830"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salaexameii-ge-nm830",
+      "name": "SalaExameII GE NM830",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.4587282464476523,
+        "pitch": 0.38338701140679987,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.005609685424128,
+          "pitch": 0.4396251754792395,
+          "rotation": 0,
+          "target": "0-salaexamei-ge-nm830"
+        },
+        {
+          "yaw": -0.43244347980985154,
+          "pitch": 0.37206211661519717,
+          "rotation": 0,
+          "target": "2-salacomando-ge-nm830"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salacomando-ge-nm830",
+      "name": "SalaComando GE NM830",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.5507435167763663,
+        "pitch": 0.35371292429226386,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2315643934939615,
+          "pitch": 0.5507983540548178,
+          "rotation": 0,
+          "target": "1-salaexameii-ge-nm830"
+        },
+        {
+          "yaw": 2.7310324680581406,
+          "pitch": 0.3101041770326116,
+          "rotation": 0,
+          "target": "0-salaexamei-ge-nm830"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "AMais Heitor Penteado 31/mai/2021",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
